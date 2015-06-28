a6a7785fc7cd2061e88c749ea36a3774cd6ad20a
Tyler Haas <haasdevelopment@gmail.com>
2015-06-27 21:15:33 -0600

    restyled the portfolio section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-$("a").click(function() {
-	$("html, body").animate({
-	  scrollTop: $( $(this).attr("href") ).offset().top
-	}, 750);
+$('a[href^="#"]').on('click', function(event){
+	var id = $(this).attr('href');
+	var target = $(id);
+
+	if (target.length) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 750);
+	}
 });
 //# sourceMappingURL=script.js.map
