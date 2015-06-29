01ea12a28c8591d796ab4cfff1a250795cbfa1fb
Tyler Haas <haasdevelopment@gmail.com>
2015-06-28 21:38:36 -0600

    added resume to site

--- src/js/hidenav.js
+++ src/js/hidenav.js
@@ -19,15 +19,3 @@ $.fn.followTo = function (pos) {
 
 console.log($('#resume').scrollTop().top);
 $('#nav-container').followTo($('#resume').offset().top);
-$('a[href^="#"]').on('click', function(event){
-	var id = $(this).attr('href');
-	var target = $(id);
-
-	if (target.length) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: target.offset().top
-		}, 750);
-	}
-});
-//# sourceMappingURL=script.js.map
---
+++ src/js/scrollto.js
@@ -0,0 +1,11 @@
+$('a[href^="#"]').on('click', function(event){
+	var id = $(this).attr('href');
+	var target = $(id);
+
+	if (target.length) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 750);
+	}
+});
