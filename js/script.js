0e8a3fa450b1643dbb245d74c3e01ae9b6f0eea6
Tyler Haas <haasdevelopment@gmail.com>
2015-08-23 20:36:04 -0600

    changed the design of site to be more inviting and made the portfolio images pop out on hover as well as removing the resume and replacing it with an about section

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-$.fn.followTo = function (pos) {
-    var $this = this,
-        $window = $(window);
-
-    $window.scroll(function (e) {
-        if ($window.width() > 767) {
-            if ($window.scrollTop() > pos) {
-                $this.css({
-                    position: 'absolute',
-                    top: pos
-                });
-            } else {
-                $this.css({
-                    position: 'fixed',
-                    top: 0
-                });
-            }
-        }
-    });
-};
-
-$('#nav-container').followTo($('#resume').offset().top);
+//$.fn.followTo = function (pos) {
+//    var $this = this,
+//        $window = $(window);
+//
+//    $window.scroll(function (e) {
+//        if ($window.width() > 767) {
+//            if ($window.scrollTop() > pos) {
+//                $this.css({
+//                    position: 'absolute',
+//                    top: pos
+//                });
+//            } else {
+//                $this.css({
+//                    position: 'fixed',
+//                    top: 0
+//                });
+//            }
+//        }
+//    });
+//};
+//
+//$('#nav-container').followTo($('#resume').offset().top);
 $('a[href^="#"]').on('click', function(event){
 	var id = $(this).attr('href');
 	var target = $(id);
--- src/js/hidenav.js
+++ src/js/hidenav.js
@@ -1,22 +1,22 @@
-$.fn.followTo = function (pos) {
-    var $this = this,
-        $window = $(window);
-
-    $window.scroll(function (e) {
-        if ($window.width() > 767) {
-            if ($window.scrollTop() > pos) {
-                $this.css({
-                    position: 'absolute',
-                    top: pos
-                });
-            } else {
-                $this.css({
-                    position: 'fixed',
-                    top: 0
-                });
-            }
-        }
-    });
-};
-
-$('#nav-container').followTo($('#resume').offset().top);
+//$.fn.followTo = function (pos) {
+//    var $this = this,
+//        $window = $(window);
+//
+//    $window.scroll(function (e) {
+//        if ($window.width() > 767) {
+//            if ($window.scrollTop() > pos) {
+//                $this.css({
+//                    position: 'absolute',
+//                    top: pos
+//                });
+//            } else {
+//                $this.css({
+//                    position: 'fixed',
+//                    top: 0
+//                });
+//            }
+//        }
+//    });
+//};
+//
+//$('#nav-container').followTo($('#resume').offset().top);
